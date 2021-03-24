fe6d3ea89e66ae0ed0f136d29a0a74556f7c876b
Clement Tagoe <clementtagoe110@gmail.com>
2021-03-24 11:04:29 +0000

    Add other pages

--- script.js
+++ script.js
@@ -6,18 +6,6 @@ var myNav = document.getElementById('mynav');
 
 window.onscroll = function () { 
     "use strict";
-    if (document.body.scrollTop >= 1200 || document.documentElement.scrollTop >= 1200 ) {
-        firstNum.classList.add("count1");
-        secNum.classList.add("count1");
-        thirdNum.classList.add("count33");
-        fourthNum.classList.add("count44");
-    } else {
-        firstNum.classList.remove("count1");
-        secNum.classList.remove("count1");
-        thirdNum.classList.remove("count33");
-        fourthNum.classList.remove("count44");
-    }
-
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
         myNav.classList.add("nav-colored");
         myNav.classList.remove("nav-transparent");
@@ -26,17 +14,21 @@ window.onscroll = function () {
         myNav.classList.add("nav-transparent");
         myNav.classList.remove("nav-colored");
     }
+
+    if (document.body.classList.contains("body")) {
+    
+        if (document.body.scrollTop >= 1200 || document.documentElement.scrollTop >= 1200) {
+            firstNum.classList.add("count1");
+            secNum.classList.add("count1");
+            thirdNum.classList.add("count33");
+            fourthNum.classList.add("count44");
+        } 
+        else {
+            firstNum.classList.remove("count1");
+            secNum.classList.remove("count1");
+            thirdNum.classList.remove("count33");
+            fourthNum.classList.remove("count44");
+        }
+    
+    }
 };
-
-
-// window.onscroll = function () { 
-//     "use strict";
-//     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
-//         myNav.classList.add("nav-colored");
-//         myNav.classList.remove("nav-transparent");
-//     } 
-//     else {
-//         myNav.classList.add("nav-transparent");
-//         myNav.classList.remove("nav-colored");
-//     }
-// };
